43af83b17bc76a41a14a8c3417d705655100f1e2
thatboijosh23 <joshua.cajou277038@gmail.com>
2020-08-25 10:33:04 -0500

    created recent posts and search bar components and used them in the home component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
+import Logo from './logo';
+import SearchBar from './searchBar';
+import RecentPosts from './recentPosts';
+
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div>
         <div>
-          hello
+          <Logo/>
+          <SearchBar/>
+          <RecentPosts/>
         </div>
       </div>
     );
---
+++ src/components/logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+                <img alt="daily smarty ui image logo big" src="/assets/download.png"/>
+            </div>
+        )
+    }
+}
+
+export default Logo;
