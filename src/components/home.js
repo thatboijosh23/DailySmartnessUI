c5ce2b008fb5f2f42d5afd0bc1856add948a417b
thatboijosh23 <joshua.cajou277038@gmail.com>
2020-08-25 12:55:30 -0500

    setup app routes

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-
 import Logo from './logo';
 import SearchBar from './searchBar';
 import RecentPosts from './recentPosts';
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
       <div>
