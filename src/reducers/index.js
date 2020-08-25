5ac67cde432bbc7ccf92ae11c1c742beb52cc8c8
thatboijosh23 <joshua.cajou277038@gmail.com>
2020-08-25 16:27:18 -0500

    connect recent posts to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,6 @@
-import { combineReducers } from 'redux';
-
-import { reducer as form } from 'redux-form';
-
-const rootReducer = combineReducers({
-  form
-});
-
-export default rootReducer;
+export function fetchRecentPosts() {
+  return function(dispatch) {
+    //perform our request in here.
+    console.log('hello');
+  }
+}
